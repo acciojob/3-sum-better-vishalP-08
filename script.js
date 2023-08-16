306ada299ba49a0b692b64a82f083a0b7a14f742
Vishal.AI <132605163+vishalP-08@users.noreply.github.com>
2023-08-16 16:51:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function threeSum(arr, target) {
-// write your code here
+function threeSum(nums, target) {
+nums.sort((a, b) => a - b); // Sort the array in ascending order
+    let closestSum = Infinity;
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                closestSum = sum;
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
   
 }
 
